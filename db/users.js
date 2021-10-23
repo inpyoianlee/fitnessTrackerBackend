2bef98709df90c950240394e7bf728fb6448317c
Brian Leay <brian@brianleay.com>
2021-10-23 12:11:42 -0700

    get user is setup on user.js

--- db/users.js
+++ db/users.js
@@ -1,26 +1,39 @@
-const client = require('./client');
+const client = require("./client");
+const bcrypt = require("bcrypt");
 
-async function createUser({username, password}) {
-    const SALT_COUNT = 10;
+async function createUser({ username, password }) {
+  const SALT_COUNT = 10;
 
-    bcrypt.hash(password, SALT_COUNT, function(err, hashedPassword) {
-        createUser({
-            username,
-            password: hashedPassword // not the plaintext
-        });
-    });
+  bcrypt.hash(password, SALT_COUNT, function (err, hashedPassword) {
+    try {
+      const { rows } = await client.query(
+        `
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            RETURNING *;
+              `,
+        [username, hashedPassword]
+      );
+      return rows[0];
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
 }
 
-// inside of createUser({ username, password})
+async function getUser({ username, password }) {
+  const user = await getUserByUserName(username);
+  const hashedPassword = user.password;
 
-// // inside of getUser({username, password})
-// const user = await getUserByUserName(username);
-// const hashedPassword = user.password;
+  bcrypt.compare(password, hashedPassword, function (err, passwordsMatch) {
+    if (passwordsMatch) {
+      delete user.password;
+      return user;
+    } else {
+      throw SomeError;
+    }
+  });
+}
 
-// bcrypt.compare(password, hashedPassword, function(err, passwordsMatch) {
-//   if (passwordsMatch) {
-//     // return the user object (without the password)
-//   } else {
-//     throw SomeError;
-//   }
-// });
+module.exports = { createUser, getUser };
