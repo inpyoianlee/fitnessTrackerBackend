8a50ab3b12a5e1a837effecbf72353f8e7db61a7
inpyoianlee <65551238+inpyoianlee@users.noreply.github.com>
2021-10-25 21:28:15 -0700

    made good progress with routines/ routine_activities

--- db/routines.js
+++ db/routines.js
@@ -1,4 +1,5 @@
 const client = require("../db/client");
+const { attachActivitiesToRoutines } = require('./activities.js');
 
 async function getRoutineById(id) {
     try {
@@ -7,7 +8,7 @@ async function getRoutineById(id) {
     }
     catch (err) {
         console.log(err);
-    }   
+    }
 }
 
 async function getRoutinesWithoutActivities() {
@@ -22,7 +23,30 @@ async function getRoutinesWithoutActivities() {
 
 async function getAllRoutines() {
     try {
-        const result = await client.query(`SELECT * FROM routines;`);
-        
+        const {rows: routines } = await client.query(`
+            SELECT routines.*, users.username as "creatorName" 
+            FROM routines
+            JOIN users ON users.id=routines."creatorId";
+        `);
+        const results = await attachActivitiesToRoutines(routines);
+        return results;
+    } catch (err) {
+        throw err;
+    }
+} 
 
-module.exports = {getRoutineById,getRoutinesWithoutActivities, getAllRoutines, };
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+    try {
+        const {rows: [routine]} = await client.query(`
+            INSERT INTO routines ("creatorId", "isPublic", name, goal)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *;
+        `, [creatorId, isPublic, name, goal])
+
+        return routine;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {getRoutineById,getRoutinesWithoutActivities, getAllRoutines, createRoutine };
